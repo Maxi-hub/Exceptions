04545fbaed9234b20feca97121b9967718f4c9db
Emma <em.lebedeva85@gmail.com>
2023-10-31 14:39:06 +0300

    modified homework3

--- HomeWork3/src/Main.java
+++ HomeWork3/src/Main.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.file.Paths;
 import java.text.ParseException;
@@ -7,14 +9,24 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 import java.io.IOException;
-import static java.lang.Math.min;
 
+//Напишите приложение, которое будет запрашивать у пользователя следующие данные в произвольном порядке, разделенные пробелом:
+//Фамилия Имя Отчество дата рождения номер телефона пол
+// Форматы данных:
+//фамилия, имя, отчество - строки
+//дата рождения - строка формата dd.mm.yyyy
+//номер телефона - целое беззнаковое число без форматирования
+//пол - символ латиницей f или m.
 //Приложение должно проверить введенные данные по количеству. Если количество не совпадает с требуемым, вернуть код ошибки,
 // обработать его и показать пользователю сообщение, что он ввел меньше и больше данных, чем требуется.
-//
 //Приложение должно попытаться распарсить полученные значения и выделить из них требуемые параметры. Если форматы данных
-// не совпадают, нужно бросить исключение, соответствующее типу проблемы. Можно использовать встроенные типы java и создать
-// свои. Исключение должно быть корректно обработано, пользователю выведено сообщение с информацией, что именно неверно.
+// не совпадают, нужно бросить исключение, соответствующее типу проблемы. Можно использовать встроенные типы java и создать свои.
+// Исключение должно быть корректно обработано, пользователю выведено сообщение с информацией, что именно неверно.
+// Если всё введено и обработано верно, должен создаться файл с названием, равным фамилии, в него в одну строку должны записаться
+// полученные данные, вида
+//<Фамилия><Имя><Отчество><датарождения> <номертелефона><пол>
+// Однофамильцы должны записаться в один и тот же файл, в отдельные строки. Не забудьте закрыть соединение с файлом.
+//При возникновении проблемы с чтением-записью в файл, исключение должно быть корректно обработано, пользователь должен увидеть стектрейс ошибки.
 
 public class Main {
     public static void main(String[] args) {
@@ -45,8 +57,6 @@ public class Main {
 
         int codeResult = normalize(dates, namesOfLine);
         switch (codeResult) {
-            case 0 -> System.out.println("Информация не внесена!");
-            case -1 -> System.out.println("Проверь фамилию, имя и отчество, мало букв");
             case -2 -> System.out.println("В номере телефона не хватает цифр");
             case -3 -> System.out.println("В номере телефона слишком много циферок");
             case -4 -> System.out.println("Некорректно введен пол");
@@ -79,30 +89,35 @@ public class Main {
      }
 
     /**
+     * Проверка всех строк на отсутствие символом и проверка ФИО на количество символов
+     * @param dates список вводимых данных с консоли
+     * @param strings список наименований строк
+     */
+     static void checkSize(List<String> dates, String[] strings){
+         int i = 0;
+         for (String s : dates) {
+             if (s.isEmpty()) System.out.println("Пусто в строке \"" + strings[i] + "\"");
+             i++;
+         }
+
+         int j = 0;
+         for (String s : dates.subList(0, 3)) {
+             if (s.length() < 2) {
+                 System.out.println(strings[j] + " меньше двух символов");
+                 j++;
+             }
+         }
+     }
+
+
+    /**
      * Вывод кода ошибки
      * @param dates список вводимых данных с консоли
      * @param strings список наименований строк
      * @return числовое значение
      */
     static int normalize(List<String> dates, String[] strings) {
-        if (dates == null || dates.isEmpty()) {
-            return 0;
-        }
-
-        int i = 0;
-        for (String s : dates) {
-            if (s.isEmpty()) System.out.println("Пусто в строке \"" + strings[i] + "\"");
-            i++;
-        }
-
-        int j = 0;
-        for (String s : dates.subList(0, 3)) {
-            if (s.length() < 2) {
-                System.out.println(strings[j] + " меньше двух символов");
-                j++;
-            }
-        }
-
+        checkSize(dates, strings);
         if (dates.get(4).length() > 0 && dates.get(4).length() < 10) {
             return -2;
         }
@@ -136,7 +151,7 @@ public class Main {
                 }
 
             } catch (NumberFormatException e) {
-                System.out.println(str[i] + " содержит число");
+                System.out.println("Некорректно введены данные, " + str[i] + " содержит число");
             }
             i++;
         }
@@ -150,17 +165,14 @@ public class Main {
      * @return true or false
      * @throws NumberFormatException Неверный формат номера телефона
      */
-    static boolean checkPhoneNumber(String phoneNumber) throws NumberFormatException {
+    static void checkPhoneNumber(String phoneNumber) throws NumberFormatException {
         if (!phoneNumber.isEmpty()) {
             try {
-                boolean containsOnlyDigits = phoneNumber.matches("^[0-9]+$");
-                return true;
+                Integer.parseInt(phoneNumber);
             } catch (NumberFormatException e) {
                 System.out.println("Неверный формат номера телефона");
-                return false;
             }
         }
-        return false;
     }
 
 
@@ -185,17 +197,19 @@ public class Main {
      * Создание нового файла с названием равным фамилии после проверки, что все введенные данные корректны
      * @param dates список вводимых данных с консоли
      */
-    // При возникновении проблемы с чтением-записью в файл, исключение должно быть корректно обработано,
-    // пользователь должен увидеть стектрейс ошибки.
+
     static void createNewFile(List<String> dates) {
         String fileName = dates.get(0);
         try (FileWriter fileWriter = new FileWriter(Paths.get(fileName).toFile(), true)) {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            br.readLine();
             fileWriter.append("\n");
             for (var i : dates) {
                 fileWriter.write("<" + i + ">");
                 fileWriter.write(" ");
             }
-        } catch (IOException e) {
+        } catch (IOException e){
+            System.err.println("Файл не создан, в введенных данных есть ошибки");
             e.printStackTrace();
         }
     }
